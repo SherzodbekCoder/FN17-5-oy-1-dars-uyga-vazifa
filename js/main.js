225001a7232f1d5c51dcaad7de9d0227067d7b46
asosiymenu007 <asosiymenu007@gmail.com>
2024-02-08 18:44:57 +0500

    uyga vazifa jala

---
+++ js/main.js
@@ -0,0 +1,6 @@
+
+// function TelefonRaqam(num) {
+//   const kodlari = [90, 91, 99, 95, 93, 94, 97, 77, 33, 55, 71, 77, 78, 88];
+//   const boshlanishi = [+998](0-9);
+//   const raqam = num.substring(/);
+// }
